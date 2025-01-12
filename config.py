97fde5911dad9d7bdab23980c056217892d20a45
Yarramsetti Naresh <ynareshkalyan@gmail.com>
2025-01-12 10:54:25 +0530

    cr

--- config.py
+++ config.py
@@ -19,27 +19,25 @@ logging.basicConfig(
 car_parts_vs_category_graph_config = load_yaml_config('carparts_vs_category_graph_config.yaml')
 conf= load_yaml_config('conf.yaml')
 
-CNN_IMG_SIZE = (256, 256)
-#    *****      Testing
+# Testing
 TEST_FOLDER = conf["TEST_FOLDER"]  # model will read images from a test folder
 OUTPUT_CSV_PATH = conf["OUTPUT_CSV_PATH"] # store prediction in a pandas dataframe.
 
-#       *********       3. Evalute
-
+# Evaluation
 CAR_PARTS_SEG_TF_LIGHT_MODEL_PATH = conf["CAR_PARTS_SEG_TF_LIGHT_MODEL_PATH"]
 CLASSIFY_MODE_PATH = conf["CLASSIFY_MODE_PATH"]
 CAR_PARTS_SEG_MODEL_CONFIDENCE_THRESHOLD = float(conf["CAR_PARTS_SEG_MODEL_CONFIDENCE_THRESHOLD"])
 CLASSIFY_MODEL_CONFIDENCE_THRESHOLD = float(conf["CLASSIFY_MODEL_CONFIDENCE_THRESHOLD"])
 CAR_PARTS_SEG_MODEL_IOU = float(conf["CAR_PARTS_SEG_MODEL_IOU"])
 
-#          *********    2. Model Training     ************
-
+# Model Training
 DATASET_PATH = conf["DATASET_PATH"]
 CAR_PARTS_SEG_MODEL_TRAINING_EPOCHS = int(conf["CAR_PARTS_SEG_MODEL_TRAINING_EPOCHS"])
 CAR_PARTS_SEG_MODEL_BATCH = int(conf["CAR_PARTS_SEG_MODEL_BATCH"])
 CLASSIFY_MODEL_EPOCHS = int(conf["CLASSIFY_MODEL_EPOCHS"])
 CLASSIFY_MODEL_BATCH = int(conf["CLASSIFY_MODEL_BATCH"])
 
+# dataset preparation
 CAR_PARTS_NAMES = car_parts_vs_category_graph_config["major_parts"]
 CAR_PARTS_NAMES_IDX_MAPP = dict([(v, k) for k, v in CAR_PARTS_NAMES.items()])
 CATEGORY_RULES = car_parts_vs_category_graph_config["CATEGORY_RULES"]
@@ -47,13 +45,6 @@ CATEGORY_MAPP = car_parts_vs_category_graph_config["CATEGORY_MAPP"]
 CATEGORY_MAPP_IDX_MAPP = dict([(v,k) for k,v in CATEGORY_MAPP.items()])
 MAJOR_PARTS_NO_DIRECTION = car_parts_vs_category_graph_config["MAJOR_PARTS_NO_DIRECTION"]
 MAJOR_PARTS_NO_DIRECTION_IDX_MAP = dict([(v, k) for k, v in MAJOR_PARTS_NO_DIRECTION.items()])
-# all_parts = set()
-# for k,v in config["major_parts"].items():
-#     # for _,p in v.items():
-#         # for pp in p.split(" "):
-#     all_parts.add(v.replace("left","").replace("right",""))
-# for i, p in zip(range(len(all_parts)), all_parts):
-#     print(f"{i} : {p}")
 
 ############
 is_poly = True
